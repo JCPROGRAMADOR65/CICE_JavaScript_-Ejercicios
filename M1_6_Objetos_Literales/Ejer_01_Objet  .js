8409fc1f2ace80b7a9c77b13b00b23674932fcb5
JCPROGRAMADOR65 <jcrino@gmail.com>
2022-07-04 18:25:20 +0200

    M1_6_Objetos_Literales

---
+++ M1_6_Objetos_Literales/Ejer_01_Objet  .js
@@ -0,0 +1,13 @@
+/*1. Crea un objeto coche con las siguientes propiedades:
+1.1. Color → rojo
+1.2. Marca → Ford
+1.3. Modelo → Focus
+1.4. Motor → cilindros: 4, capacidad: 2.2
+*/
+let coche = {
+    color: "rojo",
+    marca: "ford",
+    modelo: "focus",
+    motor: {cilindro: "4",capacidad: "2.2"// objeto anidado. //
+   }
+}
---
+++ M1_6_Objetos_Literales/Ejer_02_Objet .js
@@ -0,0 +1,14 @@
+
+/*-2. Crea un objeto con tu nombre y tu edad y muéstralo por consola. Después añade tu
+altura y muestra de nuevo el objeto.
+*/
+
+let jc = {
+  nombre: "Juan Carlos",
+  edad: 35
+ }
+ console.log(jc)
+ jc.altura = 1.80
+
+ console.log(jc)
+
